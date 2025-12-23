b74dca94d371e2e39abb4c663fb84c35be8dac85
renal4452 <renal4452@gmail.com>
2025-12-23 22:29:00 +0700

    Update Ci & Model

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,6 +17,17 @@ from sklearn.model_selection import ParameterGrid
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # ==============================
+# INIT DAGSHUB + MLFLOW
+# ==============================
+dagshub.init(
+    repo_owner="renal4452",
+    repo_name="smsml-mlflow-bank",
+    mlflow=True
+)
+
+mlflow.set_experiment("bank-deposit-tuning")
+
+# ==============================
 # LOAD DATA (PREPROCESSED)
 # ==============================
 X_train = pd.read_csv("bank_preprocessing/X_train.csv")
@@ -86,4 +97,3 @@ for params in ParameterGrid(param_grid):
         mlflow.log_artifact("prediction_sample.csv")
 
         print(f"RUN DONE | C={params['C']} | ACC={acc:.4f}")
-
